439bd6e5645ef436d8eda0b8c1e13ac9b4e766dd
Mike <mike.slinkman@gmail.com>
2023-02-02 19:36:13 +0100

    Fix autoloading, and service provider

---
+++ src/InfluxServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Crispcode\LaravelInfluxDB;
+
+use Illuminate\Support\ServiceProvider;
+
+class InfluxServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
