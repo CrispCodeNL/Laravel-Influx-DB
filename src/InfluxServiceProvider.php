c6d783da74151f401316693f532499359f04c3e9
Mike <mike.slinkman@gmail.com>
2023-02-02 20:56:14 +0100

    Setup config and command publishing

--- src/InfluxServiceProvider.php
+++ src/InfluxServiceProvider.php
@@ -3,16 +3,27 @@
 namespace Crispcode\LaravelInfluxDB;
 
 use Illuminate\Support\ServiceProvider;
+use Crispcode\LaravelInfluxDB\Console\InfluxPersister;
 
 class InfluxServiceProvider extends ServiceProvider
 {
+    public const LOG_LOCATION = 'logs/influx.log';
+
     public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'influx-db');
     }
 
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/config/config.php' => config_path('influx.php'),
+            ], 'config');
 
+            $this->commands([
+                InfluxPersister::class
+            ]);
+        }
     }
 }
